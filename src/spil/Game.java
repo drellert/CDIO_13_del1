746b4f61661bb7aeca4694e8023772ebe9fa3cd1
Cecilie Drejer <ceciliek.drejer@gmail.com>
2018-10-05 13:04:20 +0200

    Ændret scanner navn til "input"

--- src/spil/Game.java
+++ src/spil/Game.java
@@ -7,17 +7,17 @@ public class Game {
     public static void main(String[] args) {
         Player player1 = new Player();
         Player player2 = new Player();
-        Scanner jens = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
         System.out.println("Press enter to roll the dice.");
 
         do {
-            jens.nextLine();
+            input.nextLine();
 
             //Player 1's tur:
             player1.roll(1);
 
-            jens.nextLine();
+            input.nextLine();
 
             //Player 2's tur
             player2.roll(2);
