fd61b102aa66ee6af5ff4fb162bc25ad6c237959
Erlend Tyrmi <erlend@tyrmi.com>
2018-10-05 11:04:50 +0200

    Terningtest kopieret fra i mandags!

--- src/spil/Terningespil.java
+++ src/spil/Terningespil.java
@@ -3,7 +3,7 @@ package spil;
 public class Terningespil {
 
         public static void main(String[] args) {
-            // write your code here
+            //
             // bøh
         }
     }
