ebccea1d1bb6c9dbd6766a36cf79bd2bbfb0f941
Erlend Tyrmi <erlend@tyrmi.com>
2018-10-05 14:29:18 +0200

    Test 3 af dice. Tjecker at dobbelt 1 ikke giver gevinst..

--- src/test/PlayerTest.java
+++ src/test/PlayerTest.java
@@ -15,9 +15,15 @@ public class PlayerTest {
 
     @Test
     public void didWintest() {
+        // Test, at dobbelt slag først fører til sejr, når man er over 40 points
         Player snoop = new Player();
         snoop.points = 36;
         snoop.lastroll = Optional.of(new DiceRoll(6,6));
+        assert(snoop.didWin() == false);
+
+        // Test at man taber ved dobbelt 1, selv om man er over de 40 points
+        snoop.points = 40;
+        snoop.lastroll = Optional.of(new DiceRoll(1,1));
         assert(snoop.didWin() == false);
     }
 }
